24a229c43b7d0022bd2c5e3ce23a3023985bf327
benjamin d ward <benstopics@gmail.com>
2016-04-26 15:30:32 -0400

    init

---
+++ aspdotnet-final/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspdotnet_final.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Testing testing";
+
+            return View();
+        }
+    }
+}
